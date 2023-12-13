05fa2ef4940c1c66d6d4498c37d53e6301e82c94
shendel <im_shendel@inbox.ru>
2023-12-13 21:29:56 +0300

    change setup script

--- pm2_setup.sh
+++ pm2_setup.sh
@@ -1,8 +1,14 @@
-﻿#!/bin/bash
-npm i pm2 -g
+﻿npm i pm2 -g
 echo "Setup PM2 services"
+
+chmod +x ./pm2_crawl.sh
+chmod +x ./pm2_back.sh
+chmod +x ./pm2_front.sh
+
 pm2 start pm2_crawl
 pm2 start pm2_back
 pm2 start pm2_front
+
 pm2 save
+
 echo "Ready..."
